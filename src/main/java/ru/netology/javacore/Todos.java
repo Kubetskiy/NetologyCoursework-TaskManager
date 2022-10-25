cdd99efeb89f76d91f29248810edd94f89cb4d3a
Sergey Kubetskiy <kubetskiy@gmail.com>
2022-10-25 09:11:17 +0300

    Initial commit

--- src/main/java/ru/netology/javacore/Todos.java
+++ src/main/java/ru/netology/javacore/Todos.java
@@ -1,21 +1,100 @@
 package ru.netology.javacore;
 
 import java.util.*;
-import java.util.stream.Collectors;
 
+/**
+ Сам список дел храним в toDoList
+ Историю операций храним в стеке operationStack
+ */
 public class Todos {
-    //...
 
+    private static final int MAXIMUM_TASKS = 7;
+    private final TreeSet<String> toDoList = new TreeSet<>();
+    private final Deque<Pair> operationStack = new LinkedList<>();
+
+    /**
+     @param task Название добавляемой задачи<br>
+     В случае успешного добавления (было место в списке), операция добавляется в стек
+     */
     public void addTask(String task) {
-        //...
+        if (addTaskWithoutWritingToTheStack(task)) {
+            operationStack.push(new Pair(Operation.ADD, task));
+        }
     }
 
+    /**
+     Проверяем наличие места в списке, добавляем, возвращаем тру, если место было
+     * @param task
+     * @return
+     */
+    private boolean addTaskWithoutWritingToTheStack(String task) {
+        if (toDoList.size() < MAXIMUM_TASKS) {
+            toDoList.add(task);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     @param task Название удаляемой задачи.<br>
+     В случае успешного удаления операция добавляется в стек
+     */
     public void removeTask(String task) {
-        //...
+        if (removeTaskWithoutWritingToTheStack(task)) {
+            operationStack.push(new Pair(Operation.REMOVE, task));
+        }
     }
 
+    /**
+     Проверяем наличие задачи, удаляем и возвращаем тру, если она была
+     * @param task
+     * @return
+     */
+    private boolean removeTaskWithoutWritingToTheStack(String task) {
+        if (toDoList.contains(task)) {
+            toDoList.remove(task);
+            return true;
+        }
+        return false;
+    }
+
+    /**
+     Достаем из стека пару операция - задача<br>
+     Вызываем обратную операцию для задачи
+     */
+    public void restoreTask() {
+        System.out.println(" ");
+        if (!operationStack.isEmpty()) {
+            var pair = operationStack.pop();
+            switch (pair.operation) {
+                case REMOVE -> addTaskWithoutWritingToTheStack(pair.task);
+                case ADD -> removeTaskWithoutWritingToTheStack(pair.task);
+            }
+        }
+    }
+
+    /**
+     @return Возвращает список дел, разделенных пробелами, в одну строку
+     */
     public String getAllTasks() {
-        //...
+        var stringBuilder = new StringBuilder();
+        for (String s : toDoList) {
+            stringBuilder.append(s);
+            stringBuilder.append(" ");
+        }
+        return stringBuilder.toString().trim();
     }
 
+    /**
+     Вспомогательный класс для хранения пар операция - задача в стеке
+     */
+    private class Pair {
+        Operation operation;
+        String task;
+
+        public Pair(Operation operation, String task) {
+            this.operation = operation;
+            this.task = task;
+        }
+    }
 }
--- src/test/java/ru/netology/javacore/TodosTests.java
+++ src/test/java/ru/netology/javacore/TodosTests.java
@@ -1,6 +1,65 @@
 package ru.netology.javacore;
 
-public class TodosTests {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 
-    // ваши тесты для класса Todos
+public class TodosTests {
+    Todos todos;
+
+    @BeforeEach
+    void initializeData() {
+        todos = new Todos();
+    }
+
+    @Test
+    @DisplayName("Повторяем пример из задания, лоджен быть результат \"Вторая Первый\"")
+    public void someTransactionMustHaveExpectedResult() {
+        todos.addTask("Первая");
+        todos.addTask("Вторая");
+        todos.removeTask("Первая");
+        todos.addTask("Третья");
+        todos.restoreTask();
+        todos.restoreTask();
+        Assertions.assertEquals("Вторая Первая", todos.getAllTasks());
+    }
+
+    @Test
+    @DisplayName("It is impossible to add tasks above the maximum")
+    public void itIsImpossibleToAddTasksAboveTheMaximum() {
+        // TODO Заданием установлен размер списка в 7, при изменении константы тест перестанет работать
+        for (Integer i = 0; i < 7; i++) {
+            todos.addTask(i.toString());
+        }
+        Assertions.assertEquals("0 1 2 3 4 5 6", todos.getAllTasks());
+        todos.addTask("8");
+        Assertions.assertEquals("0 1 2 3 4 5 6", todos.getAllTasks());
+    }
+
+    @Test
+    @DisplayName("Метод возвращает все задачи через пробел в отсортированном лексикографическом порядке")
+    public void theMethodReturnsAllTasksSeparatedBySpaceInSortedOrder() {
+        todos.addTask("Пробежка");
+        todos.addTask("Акробатика");
+        todos.addTask("Учёба");
+        Assertions.assertEquals("Акробатика Пробежка Учёба", todos.getAllTasks());
+    }
 }
+/*
+        var s1 = "{ \"type\": \"ADD\", \"task\": \"Первая\" }";
+        var s2 = "{ \"type\": \"ADD\", \"task\": \"Вторая\" }";
+        var s3 = "{ \"type\": \"REMOVE\", \"task\": \"Первая\" }";
+        var s4 = "{ \"type\": \"ADD\", \"task\": \"Третья\" }";
+        var s5 = "{ \"type\": \"RESTORE\" }";
+        var s6 = "{ \"type\": \"RESTORE\" }";
+        var requestTask = gson.fromJson(s2, RequestTask.class);
+        System.out.println(" ");
+
+{ "type": "ADD", "task": "Первая" }
+{ "type": "ADD", "task": "Вторая" }
+{ "type": "REMOVE", "task": "Первая" }
+{ "type": "ADD", "task": "Третья" }
+{ "type": "RESTORE" }
+{ "type": "RESTORE" }
+ */
